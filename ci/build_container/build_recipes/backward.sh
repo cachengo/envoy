ae79933ad127b973d5d3be470a3090249773fa30
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2019-03-09 20:17:44 +0000

    build_recipes: backward: Add aarch64 support
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- ci/build_container/build_recipes/backward.sh
+++ ci/build_container/build_recipes/backward.sh
@@ -7,3 +7,17 @@ VERSION=1.3
 wget -O backward-cpp-$VERSION.tar.gz https://github.com/bombela/backward-cpp/archive/v$VERSION.tar.gz
 tar xf backward-cpp-$VERSION.tar.gz
 cp backward-cpp-$VERSION/backward.hpp $THIRDPARTY_BUILD/include
+cd $THIRDPARTY_BUILD/include
+cat <<-EOF | patch -p0
+--- backward.hpp
++++ backward.hpp
+@@ -1951,6 +1951,8 @@
+ 		error_addr = reinterpret_cast<void*>(uctx->uc_mcontext.gregs[REG_EIP]);
+ #elif defined(__arm__)
+ 		error_addr = reinterpret_cast<void*>(uctx->uc_mcontext.arm_pc);
++#elif defined(__aarch64__)
++		error_addr = reinterpret_cast<void*>(uctx->uc_mcontext.pc);
+ #elif defined(__ppc__) || defined(__powerpc) || defined(__powerpc__) || defined(__POWERPC__)
+ 		error_addr = reinterpret_cast<void*>(uctx->uc_mcontext.regs->nip);
+ #else
+EOF
