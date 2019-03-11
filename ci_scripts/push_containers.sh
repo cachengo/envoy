fc0b31eea91a05fc1cc2431165147b5275b6a439
Jimmy Lafontaine Rivera <lafonj@gmail.com>
2019-03-11 16:55:50 -0400

    Fix change build dir and only build

--- ci_scripts/push_containers.sh
+++ ci_scripts/push_containers.sh
@@ -2,11 +2,11 @@ export IMAGE_TAG=$(cat VERSION)
 export AARCH=`uname -m`
 export IMAGE_NAME=envoy
 
-if [ $AARCH=="aarch64" ]; then
+if [ "$(uname -m)" == 'aarch64' ]; then
   cd ci/build_container
   TRAVIS_COMMIT=bazel052 ./docker_build.sh
   cd ../..
-  IMAGE_ID=bazel052 ENVOY_DOCKER_BUILD_DIR="./envoy-docker" ./ci/run_envoy_docker.sh \
+  IMAGE_ID=bazel052 ENVOY_DOCKER_BUILD_DIR="$(pwd)/envoy-docker" ./ci/run_envoy_docker.sh \
     './ci/do_ci.sh bazel.release.server_only'
   mkdir -p build_release
   cp ./envoy-docker-build/envoy/source/exe/envoy build_release/
