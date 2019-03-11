e3ddc895ebe641b47527ccdcb2f02ea6fc3fb40c
Jimmy Lafontaine Rivera <lafonj@gmail.com>
2019-03-11 15:42:09 -0400

    Go back to building the Envoy way

--- ci_scripts/push_containers.sh
+++ ci_scripts/push_containers.sh
@@ -2,5 +2,14 @@ export IMAGE_TAG=$(cat VERSION)
 export AARCH=`uname -m`
 export IMAGE_NAME=envoy
 
+cd ci/build_container
+TRAVIS_COMMIT=bazel052 ./docker_build.sh
+cd ../..
+IMAGE_ID=bazel052 ./ci/run_envoy_docker.sh \
+  './ci/do_ci.sh bazel.release.server_only'
+mkdir -p build_release
+cp /tmp/envoy-docker-build/envoy/source/exe/envoy build_release/
+
+
 docker build -t cachengo/$IMAGE_NAME-$AARCH:$IMAGE_TAG -f ci/Dockerfile-envoy-image .
 docker push cachengo/$IMAGE_NAME-$AARCH:$IMAGE_TAG
