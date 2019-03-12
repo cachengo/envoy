088e2ef7b756b2f15c1dac64aafce5c14a9e1f44
Jimmy Lafontaine Rivera <lafonj@gmail.com>
2019-03-12 08:15:37 -0400

    Fix

--- ci_scripts/push_containers.sh
+++ ci_scripts/push_containers.sh
@@ -6,10 +6,10 @@ if [ "$(uname -m)" == 'aarch64' ]; then
   cd ci/build_container
   TRAVIS_COMMIT=bazel052 ./docker_build.sh
   cd ../..
-  IMAGE_ID=bazel052 ENVOY_DOCKER_BUILD_DIR="/tmp/envoy-docker" ./ci/run_envoy_docker.sh \
+  IMAGE_ID=bazel052 ENVOY_DOCKER_BUILD_DIR="${WORKSPACE}@tmp/envoy-docker" ./ci/run_envoy_docker.sh \
     './ci/do_ci.sh bazel.release.server_only'
   mkdir -p build_release
-  cp /tmp/envoy-docker/envoy/source/exe/envoy build_release/
+  cp ${WORKSPACE}@tmp/envoy-docker/envoy/source/exe/envoy build_release/
   rm -rf /tmp/envoy-docker
   docker build -t cachengo/$IMAGE_NAME-$AARCH:$IMAGE_TAG -f ci/Dockerfile-envoy-image .
 else
